4ac613b967446664342fc76a3baa0c1c91b1a39a
Rafael Júnior Ribeiro <rrafaeljuniorr@gmail.com>
2021-06-29 22:18:32 -0300

    teste

--- etl/dags/censo_escolar.py
+++ etl/dags/censo_escolar.py
@@ -48,7 +48,7 @@ def get_cluster_config():
     node_pool = NodePool(
         name="extraction-pool",
         config=node_pool_config,
-        initial_node_count=0,
+        initial_node_count=1,
         autoscaling=node_pool_auto_scaling
 
     )
@@ -64,7 +64,6 @@ def get_cluster_config():
         name="extraction-cluster",
         location="southamerica-east1-a",
         node_pools=[node_pool],
-        initial_node_count=2,
         autoscaling=cluster_auto_scaling
     )
 
@@ -138,37 +137,37 @@ with DAG(dag_id="censo-escolar", default_args=args, start_date=days_ago(2)) as d
         body=get_cluster_config()
     )
 
-    # with TaskGroup(group_id="extract-files") as extract_files:
-    #     years_not_in_bronze_bucket = '{{ ti.xcom_pull(task_ids="check-bronze-bucket", key="years_not_in_bucket") }}'
-    #     for year in YEARS:
-    #         if str(year) not in years_not_in_bronze_bucket:
-    #             extract_file = GKEStartPodOperator(
-    #                 task_id=f"extract-file-{year}",
-    #                 project_id=PROJECT,
-    #                 location="southamerica-east1-a",
-    #                 cluster_name="extraction-cluster",
-    #                 namespace="default",
-    #                 image=f"gcr.io/{PROJECT}/censo_escolar:latest",
-    #                 arguments=["sh", "-c", f'python extract.py {year}'],
-    #                 env_vars={
-    #                     "DATA_LAKE": DATA_LAKE,
-    #                     "GOOGLE_APPLICATION_CREDENTIALS": "/var/secrets/google/key.json"
-    #                 },
-    #                 resources=get_pod_resources(),
-    #             #    secrets=[get_secret()],
-    #                 name=f"extract-file-{year}",
-    #                 #on_failure_callback=extract_file_error_callback,
-    #                 get_logs=True,
-    #                 startup_timeout_seconds=600
-    #                 #is_delete_operator_pod=True,
-    #             )
-    #
-    # destroy_gke_cluster = GKEDeleteClusterOperator(
-    #     task_id="destroy-gke-cluster",
-    #     name="extraction-cluster",
-    #     project_id=PROJECT,
-    #     location="southamerica-east1-a"
-    # )
+    with TaskGroup(group_id="extract-files") as extract_files:
+        years_not_in_bronze_bucket = '{{ ti.xcom_pull(task_ids="check-bronze-bucket", key="years_not_in_bucket") }}'
+        for year in YEARS:
+            if str(year) not in years_not_in_bronze_bucket:
+                extract_file = GKEStartPodOperator(
+                    task_id=f"extract-file-{year}",
+                    project_id=PROJECT,
+                    location="southamerica-east1-a",
+                    cluster_name="extraction-cluster",
+                    namespace="default",
+                    image=f"gcr.io/{PROJECT}/censo_escolar:latest",
+                    arguments=["sh", "-c", f'python extract.py {year}'],
+                    env_vars={
+                        "DATA_LAKE": DATA_LAKE,
+                        "GOOGLE_APPLICATION_CREDENTIALS": "/var/secrets/google/key.json"
+                    },
+                    resources=get_pod_resources(),
+                #    secrets=[get_secret()],
+                    name=f"extract-file-{year}",
+                    #on_failure_callback=extract_file_error_callback,
+                    get_logs=True,
+                    startup_timeout_seconds=600
+                    #is_delete_operator_pod=True,
+                )
+
+    destroy_gke_cluster = GKEDeleteClusterOperator(
+        task_id="destroy-gke-cluster",
+        name="extraction-cluster",
+        project_id=PROJECT,
+        location="southamerica-east1-a"
+    )
     # #
     # check_extractions = BranchPythonOperator(
     #     task_id="check-extractions",
@@ -185,7 +184,7 @@ with DAG(dag_id="censo-escolar", default_args=args, start_date=days_ago(2)) as d
     #     task_id="check-silver-bucket"
     # )
 
-    check_bronze_bucket >> create_gke_cluster #>> extract_files >> destroy_gke_cluster
+    check_bronze_bucket >> create_gke_cluster >> extract_files >> destroy_gke_cluster
     # check_extractions >> some_failed_extraction
     # check_extractions >> check_silver_bucket
 
