c62a403101a0d736938f0cd9b6e093b4390e053c
Rafael Júnior Ribeiro <rrafaeljuniorr@gmail.com>
2021-07-24 14:52:52 -0300

    reextracao 2016

--- infra/docker.tf
+++ infra/docker.tf
@@ -1,4 +1,4 @@
-resource "docker_registry_image" "container-extraction_" {
+resource "docker_registry_image" "container-extraction" {
 name = "gcr.io/${var.project}/censo_escolar:latest"
 build {
 context = "../etl/extraction"
