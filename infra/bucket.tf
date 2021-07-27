ffe26295259ce94e9d7b412670d4ceb485f83674
Rafael Junior Ribeiro <rrafaeljuniorr@gmail.com>
2021-07-27 23:09:00 +0000

    upload de arquivos de etl na gcs

--- infra/bucket.tf
+++ infra/bucket.tf
@@ -4,3 +4,10 @@ resource "google_storage_bucket" "data-lake" {
   force_destroy = false
 }
 
+resource "null_resource" "upload_files_" {
+    provisioner "local-exec" {
+        command = "gsutil cp -r ../etl/* gs://${google_storage_bucket.data-lake.name}/etl"
+        interpreter = ["/bin/bash", "-c"]
+    }
+}
+
