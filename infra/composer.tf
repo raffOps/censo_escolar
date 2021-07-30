1d7502ada8c5933e6cb70e2f0d82c7ea73f91f36
Rafael Júnior Ribeiro <rrafaeljuniorr@gmail.com>
2021-07-30 19:29:29 -0300

    adicionado dataproc dev

--- infra/composer.tf
+++ infra/composer.tf
@@ -1,27 +1,27 @@
-resource "google_composer_environment" "composer" {
-  name = "airflow"
-  region = "us-central1"
-    config {
-    node_count = 3
-
-    node_config {
-      zone         = "us-central1-a"
-      machine_type = "e2-medium"
-    }
-      software_config {
-        image_version = "composer-1.17.0-preview.2-airflow-2.0.1"
-        env_variables = {
-          
-          AIRFLOW_VAR_DATA_LAKE_LANDING: google_storage_bucket.landing.name
-          AIRFLOW_VAR_DATA_LAKE_PROCESSING: google_storage_bucket.processing.name
-          AIRFLOW_VAR_DATA_LAKE_CONSUMER: google_storage_bucket.consumer.name
-          AIRFLOW_VAR_DATA_LAKE_SCRIPTS: google_storage_bucket.scripts.name
-
-          AIRFLOW_VAR_FIRST_YEAR: 2011
-          AIRFLOW_VAR_LAST_YEAR: 2020
-          AIRFLOW_VAR_PROJECT: var.project
-        }
-      }
-  }
-}
-
+//resource "google_composer_environment" "composer" {
+//  name = "airflow"
+//  region = "us-central1"
+//    config {
+//    node_count = 3
+//
+//    node_config {
+//      zone         = "us-central1-a"
+//      machine_type = "e2-medium"
+//    }
+//      software_config {
+//        image_version = "composer-1.17.0-preview.2-airflow-2.0.1"
+//        env_variables = {
+//
+//          AIRFLOW_VAR_DATA_LAKE_LANDING: google_storage_bucket.landing.name
+//          AIRFLOW_VAR_DATA_LAKE_PROCESSING: google_storage_bucket.processing.name
+//          AIRFLOW_VAR_DATA_LAKE_CONSUMER: google_storage_bucket.consumer.name
+//          AIRFLOW_VAR_DATA_LAKE_SCRIPTS: google_storage_bucket.scripts.name
+//
+//          AIRFLOW_VAR_FIRST_YEAR: 2011
+//          AIRFLOW_VAR_LAST_YEAR: 2020
+//          AIRFLOW_VAR_PROJECT: var.project
+//        }
+//      }
+//  }
+//}
+//
---
+++ infra/dataproc.tf
@@ -0,0 +1,68 @@
+//resource "google_service_account" "default" {
+//  account_id   = "service-account-id"
+//  display_name = "Service Account"
+//}
+
+resource "google_dataproc_cluster" "mycluster" {
+  name     = "dados-abertos"
+  region   = "us-central1"
+//  graceful_decommission_timeout = "120s"
+//  labels = {
+//    foo = "bar"
+//  }
+
+  cluster_config {
+    staging_bucket = "dataproc-staging-bucket"
+
+    master_config {
+      num_instances = 1
+      machine_type  = "n1-highmem-4"
+      disk_config {
+        boot_disk_type    = "pd-ssd"
+        boot_disk_size_gb = 30
+      }
+    }
+
+    worker_config {
+      num_instances    = 4
+      machine_type     =  "n1-highmem-4"
+      #min_cpu_platform = "Intel Skylake"
+      disk_config {
+        boot_disk_size_gb = 30
+        num_local_ssds    = 1
+      }
+    }
+
+//    preemptible_worker_config {
+//      num_instances = 0
+//    }
+
+    # Override or set some custom properties
+    software_config {
+      image_version = "2.0-debian10"
+      override_properties = {
+        "dataproc:dataproc.allow.zero.workers" = "true"
+      }
+      optional_components = ["JUPYTER"]
+    }
+
+    endpoint_config {
+      enable_http_port_access = true
+    }
+
+//    gce_cluster_config {
+//      tags = ["foo", "bar"]
+//      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+//      service_account = google_service_account.default.email
+//      service_account_scopes = [
+//        "cloud-platform"
+//      ]
+//    }
+
+    # You can define multiple initialization_action blocks
+//    initialization_action {
+//      script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
+//      timeout_sec = 500
+//    }
+  }
+}
