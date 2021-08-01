ebb6f874309f45aa1fd0d0f0489d06d6bf8e1fdb
Rafael Júnior Ribeiro <rrafaeljuniorr@gmail.com>
2021-08-01 14:57:59 -0300

    teste

--- etl/dags/censo_escolar.py
+++ etl/dags/censo_escolar.py
@@ -14,7 +14,10 @@ from airflow.providers.google.cloud.operators.kubernetes_engine import (
     GKECreateClusterOperator,
     GKEDeleteClusterOperator
 )
-from airflow.providers.google.cloud.operators.dataproc import DataprocInstantiateWorkflowTemplateOperator
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocInstantiateWorkflowTemplateOperator,
+    DataprocCreateWorkflowTemplateOperator
+)
 from kubernetes.client import V1ResourceRequirements
 from google.cloud import storage
 
@@ -30,66 +33,6 @@ SCRIPTS_BUCKET = f"{PROJECT}-scripts"
 YEARS = list(range(FIRST_YEAR, LAST_YEAR + 1))
 
 NOW = datetime.now().isoformat()
-
-
-def calculate_cluster_size(amount_years):
-    return ceil(int(amount_years)/2) + 1
-
-
-def get_gke_cluster_def():
-    cluster_def = {
-        "name": "censo-escolar-extraction",
-        "initial_node_count": '{{ ti.xcom_pull(task_ids="extract.check_landing_bucket", key="cluster_size") }}',
-        "location": "southamerica-east1-a",
-        "node_config": {
-            "oauth_scopes": ["https://www.googleapis.com/auth/cloud-platform"],
-            "machine_type": "e2-standard-4"
-        },
-    }
-    return cluster_def
-
-
-def get_dataproc_workflow():
-    workflow = {
-        "id": f"censo-transform-{NOW}",
-        "name": "censo-transform",
-        "placement": {
-            "cluster_name": "censo-escolar-transform",
-            "config": {
-                "master_config": {
-                    "num_instances": 1,
-                    "machine_type_uri": "n1-highmem-8"
-                },
-                "worker_config": {
-                    "num_instances": 2,
-                    "machine_type_uri": "n1-highmem-8"
-                }
-            }
-        },
-        "jobs": []
-    }
-
-    prev_job = None
-    jobs = []
-    for year_ in YEARS:
-        step_id = f"censo-transform-{year_}",
-        job = {
-            "sted_id": step_id,
-            "pyspark_job": {
-                "main_python_file_uri": f"gs://{PROCESSING_BUCKET}/censo_escolar/transformation/transform.py",
-                "args": [PROJECT, year_]
-            }
-        }
-
-        if prev_job:
-            job["prerequisite_step_ids"] = prev_job
-
-        prev_job = step_id
-        jobs.append(job)
-
-    workflow["jobs"] = jobs
-
-    return workflow
 
 
 def check_years(**context):
@@ -125,6 +68,22 @@ def check_year(**context):
         return false_option
 
 
+def calculate_cluster_size(amount_years):
+    return ceil(int(amount_years)/2) + 1
+
+
+def get_gke_cluster_def():
+    cluster_def = {
+        "name": "censo-escolar-extraction",
+        "initial_node_count": '{{ ti.xcom_pull(task_ids="extract.check_landing_bucket", key="cluster_size") }}',
+        "location": "southamerica-east1-a",
+        "node_config": {
+            "oauth_scopes": ["https://www.googleapis.com/auth/cloud-platform"],
+            "machine_type": "e2-standard-4"
+        },
+    }
+    return cluster_def
+
 def get_pod_resources():
     return V1ResourceRequirements(
         requests={
@@ -136,6 +95,55 @@ def get_pod_resources():
             "memory": "4G"
         }
     )
+
+
+def get_dataproc_workflow():
+    workflow = {
+        "id": f"censo-escolar-transform-{NOW}",
+        "name": "censo-transform",
+        "placement": {
+            "managed_cluster": {
+                "cluster_name": "censo-escolar-transform",
+                "config": {
+                    "master_config": {
+                        "num_instances": 1,
+                        "machine_type_uri": "n1-highmem-8"
+                    },
+                    "worker_config": {
+                        "num_instances": 2,
+                        "machine_type_uri": "n1-highmem-8"
+                    },
+                    "gce_cluster_config": {
+                        "zone_uri": "us-east1-b"
+                    }
+                }
+            },
+        },
+        "jobs": []
+    }
+
+    prev_job = None
+    jobs = []
+    years = json.loads('{{ ti.xcom_pull(task_ids="check_processing_bucket", key="years_not_in_this_bucket"}}')
+    for year_ in years:
+        step_id = f"censo-transform-{year_}",
+        job = {
+            "sted_id": step_id,
+            "pyspark_job": {
+                "main_python_file_uri": f"gs://{PROCESSING_BUCKET}/censo_escolar/transformation/transform.py",
+                "args": [PROJECT, year_]
+            }
+        }
+
+        if prev_job:
+            job["prerequisite_step_ids"] = prev_job
+
+        prev_job = step_id
+        jobs.append(job)
+
+    workflow["jobs"] = jobs
+
+    return workflow
 
 
 with DAG(dag_id="censo-escolar", default_args={'owner': 'airflow'}, start_date=days_ago(0)) as dag:
@@ -217,7 +225,7 @@ with DAG(dag_id="censo-escolar", default_args={'owner': 'airflow'}, start_date=d
             python_callable=check_years,
             provide_context=True,
             op_kwargs={
-                "true_option": "transform.run_dataproc_job",
+                "true_option": "transform.create_workflow_template",
                 "false_option": "transform.transformation_finished_with_sucess",
                 "bucket": PROCESSING_BUCKET,
                 "years": YEARS
@@ -225,15 +233,18 @@ with DAG(dag_id="censo-escolar", default_args={'owner': 'airflow'}, start_date=d
             trigger_rule="none_failed"
         )
 
+        create_workflow_template = DataprocCreateWorkflowTemplateOperator(
+            task_id="create_workflow_template",
+            template=get_dataproc_workflow(),
+            project_id=PROJECT,
+            location="us-east1",
+        )
+
         run_dataproc_job = DataprocInstantiateWorkflowTemplateOperator(
             task_id=f"run_dataproc_job",
-            template_id="censo-escolar-transform",
+            template_id=f"censo-escolar-transform-{NOW}",
             project_id=PROJECT,
-            region="us-east1",
-            parameters={
-                "PROJECT": PROJECT,
-                "YEARS": '{{ ti.xcom_pull(task_ids="check_processing_bucket", key="years_not_in_this_bucket_str") }}'
-            }
+            region="us-west1"
         )
 
         transformation_finished_with_sucess = DummyOperator(
@@ -241,7 +252,7 @@ with DAG(dag_id="censo-escolar", default_args={'owner': 'airflow'}, start_date=d
             trigger_rule='none_failed'
         )
 
-        check_processing_bucket >> [run_dataproc_job, transformation_finished_with_sucess]
-        run_dataproc_job >> transformation_finished_with_sucess
+        check_processing_bucket >> [create_workflow_template, transformation_finished_with_sucess]
+        create_workflow_template >> run_dataproc_job >> transformation_finished_with_sucess
 
     extract >> transform
