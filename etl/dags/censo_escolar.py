e4d9ed41d7c3fb47223b34538108df8721bf7120
Rafael Júnior Ribeiro <rrafaeljuniorr@gmail.com>
2021-08-01 19:04:14 -0300

    teste

--- etl/dags/censo_escolar.py
+++ etl/dags/censo_escolar.py
@@ -41,11 +41,10 @@ NOW = datetime.now().isoformat()
 def calculate_cluster_size(amount_years):
     return ceil(amount_years/2) + 1
 
-
 def get_gke_cluster_def():
     cluster_def = {
         "name": "censo-escolar-extraction",
-        "initial_node_count": '{{ ti.xcom_pull(task_ids="extract.check_landing_zone", key="cluster_size") }}',
+        "initial_node_count": '{{ ti.xcom_pull(task_ids="extract.check_landing_bucket", key="cluster_size") }}',
         "location": "southamerica-east1-a",
         "node_config": {
             "oauth_scopes": ["https://www.googleapis.com/auth/cloud-platform"],
