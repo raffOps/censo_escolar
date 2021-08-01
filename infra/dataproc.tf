fe941b3f400b9920a7d1f2d10ff0c1d37e7c3bee
Rafael Júnior Ribeiro <rrafaeljuniorr@gmail.com>
2021-08-01 14:34:32 -0300

    teste

--- infra/dataproc.tf
+++ infra/dataproc.tf
@@ -46,53 +46,53 @@
 # }
 
 
-resource "google_dataproc_workflow_template" "censo-escolar-transform" {
-  name = "censo-escolar-transform"
-  location = "us-east1"
-  placement {
-    managed_cluster {
-      cluster_name = "censo-escolar"
-      config {
-        gce_cluster_config {
-          zone = "us-east1-b"
-        }
-        master_config {
-          num_instances = 1
-          machine_type = "n1-highmem-8"
-          disk_config {
-            boot_disk_type = "pd-ssd"
-            boot_disk_size_gb = 100
-          }
-        }
-        worker_config {
-          num_instances = 2
-          machine_type = "n1-highmem-8"
-          disk_config {
-            boot_disk_size_gb = 100
-            num_local_ssds = 2
-          }
-        }
-
-        software_config {
-          image_version = "2.0-debian10"
-        }
-      }
-    }
-  }
-  jobs {
-    step_id = "pyspark"
-    pyspark_job {
-      main_python_file_uri = "gs://${google_storage_bucket.scripts.name}/censo_escolar/transformation/transform.py"
-      args = ["project", "year"]
-    }
-  }
-  parameters {
-    name = "PROJECT"
-    fields = ["jobs['pyspark'].pysparkJob.args[0]"]
-  }
-
-  parameters {
-    name = "YEAR"
-    fields = ["jobs['pyspark'].pysparkJob.args[1]"]
-  }
-}
+//resource "google_dataproc_workflow_template" "censo-escolar-transform" {
+//  name = "censo-escolar-transform"
+//  location = "us-east1"
+//  placement {
+//    managed_cluster {
+//      cluster_name = "censo-escolar"
+//      config {
+//        gce_cluster_config {
+//          zone = "us-east1-b"
+//        }
+//        master_config {
+//          num_instances = 1
+//          machine_type = "n1-highmem-8"
+//          disk_config {
+//            boot_disk_type = "pd-ssd"
+//            boot_disk_size_gb = 100
+//          }
+//        }
+//        worker_config {
+//          num_instances = 2
+//          machine_type = "n1-highmem-8"
+//          disk_config {
+//            boot_disk_size_gb = 100
+//            num_local_ssds = 2
+//          }
+//        }
+//
+//        software_config {
+//          image_version = "2.0-debian10"
+//        }
+//      }
+//    }
+//  }
+//  jobs {
+//    step_id = "pyspark"
+//    pyspark_job {
+//      main_python_file_uri = "gs://${google_storage_bucket.scripts.name}/censo_escolar/transformation/transform.py"
+//      args = ["project", "year"]
+//    }
+//  }
+//  parameters {
+//    name = "PROJECT"
+//    fields = ["jobs['pyspark'].pysparkJob.args[0]"]
+//  }
+//
+//  parameters {
+//    name = "YEAR"
+//    fields = ["jobs['pyspark'].pysparkJob.args[1]"]
+//  }
+//}
